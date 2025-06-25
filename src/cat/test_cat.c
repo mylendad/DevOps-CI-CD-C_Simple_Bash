71a91967591ba764c05f8787f8f4d944df27be0d
Mylenda Darci <mylendad@student.21-school.ru>
2025-06-25 22:23:21 +0300

    added pipline

---
+++ src/cat/test_cat.c
@@ -0,0 +1,18 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cat.h"
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {  // предварительная проверка аргументов функции
+    printf("Usage: %s <filename>\n", argv[0]);
+    return 0;
+  }
+  int counter = 1;
+  flags argument = parser(argc, argv, &counter);
+  int index_for_file = argc - counter;
+  output(&argument, index_for_file, &argv[counter]);
+  return 0;
+}
---
+++ src/grep/grep.h
@@ -0,0 +1,35 @@
+#ifndef GREP_H
+#define GREP_H
+
+#include <regex.h>
+#include <stdio.h>
+
+#define MAX_LINE_SIZE 1024
+
+typedef struct flags {
+  int error;
+  int len;
+  int memory;
+  char *reg_pattern;
+  int e, i, v, c, l, n, h, s, f, o;
+} flags;
+
+void add_pattern(flags *argument, char *pattern);
+
+void reader_regs(flags *argument, char *file_name);
+
+flags parser(int args, char **argv);
+
+void flag_o(flags *argument, regex_t *re, char *line, char *file_name,
+            int line_counter);
+
+void outline(char *line, int n);
+
+FILE *reader(char *file_name);
+
+void comparator(flags *argument, char *file, regex_t *reg, int argc,
+                char **argv, int count_filename);
+
+void output(flags *argument, int argc, char **argv);
+
+#endif
