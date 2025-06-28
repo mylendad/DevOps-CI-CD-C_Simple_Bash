20a94ee923316c018e382beb5609906927cfbbf0
Mylenda Darci <mylendad@student.21-school.ru>
2025-06-28 19:32:09 +0300

    fixed001 pipline. added deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+REMOTE_USER=mylendad
+REMOTE_HOST=10.0.2.15
+REMOTE_DIR=/usr/local/bin
+
+LOCAL_DIR=src/build/*
+
+scp $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
+
+echo "DEPLOY: SUCCES!"
