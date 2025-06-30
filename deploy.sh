700d40a492ba69280e6d947f130a3aba7e653be9
Mylenda Darci <mylendad@student.21-school.ru>
2025-06-30 19:51:08 +0300

    1234773

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -ex  
-
-echo "Current user: $(whoami)"
-echo "Home dir: $HOME"
-ls -la ~/.ssh
+set -ex
 
 REMOTE_USER=mylendad
 REMOTE_HOST=192.168.100.2
 REMOTE_DIR=/usr/local/bin
 LOCAL_DIR=src/build/*
 
-ssh -o BatchMode=yes ${REMOTE_USER}@${REMOTE_HOST}
+ssh -o BatchMode=yes \
+    -o UserKnownHostsFile=/home/gitlab-runner/.ssh/known_hosts \
+    ${REMOTE_USER}@${REMOTE_HOST}
 
-scp $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
+scp -o UserKnownHostsFile=/home/gitlab-runner/.ssh/known_hosts \
+    -o StrictHostKeyChecking=yes \
+    -v $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
