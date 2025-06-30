b0e2574111f9d0b14f874e4cab77ecd0624fb58a
Mylenda Darci <mylendad@student.21-school.ru>
2025-06-30 19:57:17 +0300

    12347734e33

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -ex
 
+export HOME=/home/gitlab-runner
+
 REMOTE_USER=mylendad
 REMOTE_HOST=192.168.100.2
 REMOTE_DIR=/usr/local/bin
 LOCAL_DIR=src/build/*
 
 ssh -o BatchMode=yes \
-    -o UserKnownHostsFile=/home/gitlab-runner/.ssh/known_hosts \
+    -o StrictHostKeyChecking=yes \
+    -o UserKnownHostsFile=$HOME/.ssh/known_hosts \
     ${REMOTE_USER}@${REMOTE_HOST}
 
-scp -o UserKnownHostsFile=/home/gitlab-runner/.ssh/known_hosts \
-    -o StrictHostKeyChecking=yes \
-    -v $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
+scp -o StrictHostKeyChecking=yes \
+    -o UserKnownHostsFile=$HOME/.ssh/known_hosts \
+    $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
