6157a4fbe6c1a510c068524a3735f8a56ac37afd
Mylenda Darci <mylendad@student.21-school.ru>
2025-06-30 22:01:35 +0300

    12222evfhul

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,4 @@ REMOTE_HOST=192.168.100.2
 REMOTE_DIR=/usr/local/bin
 LOCAL_DIR=src/build/*
 
-ssh -o BatchMode=yes \
-    ${REMOTE_USER}@${REMOTE_HOST}
-
 scp $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
