65bcffc0005ad215880e53bfa0bc1943b5f56434
Mylenda Darci <mylendad@student.21-school.ru>
2025-07-01 22:01:13 +0300

    finishhhhhhh

--- src/cat.h
+++ src/cat.h
@@ -1,7 +1,7 @@
 #ifndef CAT_H
 #define CAT_H
 
-#define MAX_LINE_SIZE 2011
+#define MAX_LINE_SIZE 2010
 
 typedef struct flags {
   int error;
@@ -17,3 +17,10 @@ void print(char *argv[], flags *flag);
 void output(flags *argument, int file_count, char *argv[]);
 
 #endif  // CAT_Haaa
+ssh -
+    ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxhdD0zG2qt4C4YE8kS0GgQUeC39Rx6UqikwkD6QGeU
+        gitlab -
+    runner @mylendadserver ssh -
+    ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxhdD0zG2qt4C4YE8kS0GgQUeC39Rx6UqikwkD6QGeU
+        gitlab -
+    runner @mylendadserver
