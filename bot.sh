991187094baec55cae3896bd9cfdca56050ef11f
Mylenda Darci <mylendad@student.21-school.ru>
2025-07-01 19:56:37 +0300

    finishhhhh

--- bot.sh
+++ bot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ ${2} == "1" ]] && [[ $CI_JOB_STATUS == "success" ]]; then
-    exit;
-fi
+# if [[ ${2} == "1" ]] && [[ $CI_JOB_STATUS == "success" ]]; then
+#     exit;
+# fi
 
 source /home/gitlab-runner/env.sh
 TIME=60
 set -ex
 
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Deploy status: $CI_JOB_STATUS%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
