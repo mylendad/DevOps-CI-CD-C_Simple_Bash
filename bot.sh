536c4f767e19c7b470bd257750b282ad5443b423
Mylenda Darci <mylendad@student.21-school.ru>
2025-07-01 22:51:52 +0300

    finishhhhhhhaффффураааанеne

--- bot.sh
+++ bot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# if [[ ${2} == "1" ]] && [[ $CI_JOB_STATUS == "success" ]]; then
-#     exit;\\\\
-# fi
+if [[ ${2} == "1" ]] && [[ $CI_JOB_STATUS == "success" ]]; then
+    exit;
+fi
 
 source /home/gitlab-runner/env.sh
 TIME=60
