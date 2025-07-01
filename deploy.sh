ce89dbae3a989356f35ac94783806525c7d63d18
Mylenda Darci <mylendad@student.21-school.ru>
2025-07-01 23:02:37 +0300

    finishhhhhhhaффффураааанеneura

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,5 @@ REMOTE_DIR=/usr/local/bin
 LOCAL_DIR=src/build/*
 
 scp $LOCAL_DIR ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
+
+echo "DEPLOY_JOB_STATUS=$CI_JOB_STATUS" >> /home/gitlab-runner/env.sh
